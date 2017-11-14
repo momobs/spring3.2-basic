4d40cb89e5c398fc28960ab722801569819a222f
Yongmin <hapitd@naver.com>
2017-11-14 12:06:54 +0900

    Spring Framework 3.2 Basic

---
+++ kbasic/src/main/java/kbasic/common/service/CommonService.java
@@ -0,0 +1,10 @@
+package kbasic.common.service;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface CommonService {
+	Map<String, Object> selectFileInfo(Map<String, Object> map) throws Exception;
+	
+}
